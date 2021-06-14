9ffd3375eedb37cd38673c1c212ce4a31e870cd1
richie-south <richard.soderman@gmail.com>
2021-06-14 22:24:01 +0200

    feat: config mode slice

---
+++ board/src/lib/features/config-mode/config-mode-slice.ts
@@ -0,0 +1,14 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+type ConfigModeState = boolean
+const initialState: ConfigModeState = false
+
+export const configModeSlice = createSlice({
+  name: 'configMode',
+  initialState,
+  reducers: {
+    toggleConfigMode: (state) => {
+      state = !state
+    },
+  },
+})
